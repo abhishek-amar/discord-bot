41b5ebfdf89c135f6583d962c3599af135c054d4
Abhishek <66067391+abhishek-amar@users.noreply.github.com>
2020-07-11 19:36:07 +0530

    Remove default help command

--- discord_bot.py
+++ discord_bot.py
@@ -1,10 +1,11 @@
 import discord
 from discord.ext import commands
 
-client = commands.Bot(command_prefix = '.')
+client = commands.Bot(command_prefix = '.', case_insensitive = True)
+client.remove_command('help')
 
 def get_token():
-    with open('discord-bot/token.txt', 'r') as f:
+    with open('token.txt', 'r') as f:
         f_contents = f.readline()
         return f_contents
 
@@ -14,7 +15,7 @@ async def ping(ctx):
     await ctx.send(f"Pong! API latency is {round(client.latency * 1000)} ms")
 
 
-extensions = ['command_events', 'admin_commands', 'member_commands', 'member_events', 'owner_commands']
+extensions = ['command_events', 'admin_commands', 'member_commands', 'member_events', 'owner_commands', 'help_commands']
 
 if __name__ == '__main__':
     for extension in extensions:
