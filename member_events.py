55f513e7b60b25d86e0f0de4fa8af17f1997ea22
Abhishek <66067391+abhishek-amar@users.noreply.github.com>
2020-07-16 17:42:20 +0530

    Added delete message embed

--- member_events.py
+++ member_events.py
@@ -8,6 +8,8 @@ class MemberEvents(commands.Cog):
         self.bot = bot
         self.log_channel = self.bot.get_channel(732908487717158962)
         self.welcome_channel = self.bot.get_channel(505992554697850890)
+        self.guild =  self.bot.get_guild(505981261693845514)
+
     @commands.Cog.listener()
     async def on_ready(self):
         print('Bot is ready')
@@ -29,15 +31,16 @@ class MemberEvents(commands.Cog):
         # Boolean value of an empty string is False
         if not before.content or not after.content:
             return
-
-        string_date_time = after.edited_at.strftime("%A, %B %d, %Y\n%H:%M:%S")
+        else:
+            #Prevents attribute error if after is NoneType
+            string_date_time = after.edited_at.strftime("%A, %B %d, %Y\n%H:%M:%S")
         embed4 = discord.Embed(
                 title = 'Message Edit',
                 colour = discord.Colour.blue()
                 )
         embed4.set_author(
             name = f"{before.author.name}#{before.author.discriminator}\n({before.author.id})",
-            icon_url=str(before.author.avatar_url)
+            icon_url = str(before.author.avatar_url)
         )
         embed4.add_field(name = "Channel:", value = f"{before.channel.mention}\n({before.channel.id})", inline = False)
         embed4.add_field(name = "Before:", value = before.content, inline = False)
@@ -45,6 +48,37 @@ class MemberEvents(commands.Cog):
         embed4.set_footer(text = f'Message ID: {before.id}\n{string_date_time}')
         await self.log_channel.send(embed = embed4)
     
+    @commands.Cog.listener()
+    async def on_message_delete(self, message):
+        #Prevents error if an embed is deleted (treated like an empty message)
+        if not message.content:
+            return
 
+        #Checks audit logs if a user deleted his own message or someone else's message to edit the author of embed
+        async for entry in self.guild.audit_logs(action = discord.AuditLogAction.message_delete, limit=1):
+            if entry.user != entry.target:
+                name = entry.user.name
+                disc = entry.user.discriminator
+                ident = entry.user.id
+                pic = entry.user.avatar_url
+            else:
+                name = message.author.name
+                disc = message.author.discriminator
+                ident = message.author.id
+                pic = message.author.avatar_url
+
+        now_date_time = dt.datetime.now(dt.timezone.utc)
+        string_date_time = now_date_time.strftime("%A, %B %d, %Y\n%H:%M:%S")
+        embed5 = discord.Embed(title = "Message Delete", colour = discord.Colour.red())
+        embed5.set_author(
+            name = f"{name}#{disc}\n({ident})",
+            icon_url= str(pic)
+        )
+        embed5.add_field(name = "Channel:", value = f"{message.channel.mention}")
+        embed5.add_field(name = "Author:", value = f"{message.author.name}#{message.author.discriminator}\n({message.author.id})")
+        embed5.add_field(name = "Message:", value = message.content, inline = False)
+        embed5.set_footer(text = f'Message ID: {message.id}\n{string_date_time}')
+        await self.log_channel.send(embed = embed5)
+        
 def setup(bot):
     bot.add_cog(MemberEvents(bot))
